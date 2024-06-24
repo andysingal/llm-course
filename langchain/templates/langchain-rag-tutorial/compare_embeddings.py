2363f1d0bd950f7236d733a8c113b54018b4a24e
Ankush Singal <andysingal@gmail.com>
2024-06-24 14:00:50 +0530

    Add files via upload

---
+++ langchain/templates/langchain-rag-tutorial/compare_embeddings.py
@@ -0,0 +1,29 @@
+from langchain_openai import OpenAIEmbeddings
+from langchain.evaluation import load_evaluator
+from dotenv import load_dotenv
+import openai
+import os
+
+# Load environment variables. Assumes that project contains .env file with API keys
+load_dotenv()
+#---- Set OpenAI API key 
+# Change environment variable name from "OPENAI_API_KEY" to the name given in 
+# your .env file.
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+def main():
+    # Get embedding for a word.
+    embedding_function = OpenAIEmbeddings()
+    vector = embedding_function.embed_query("apple")
+    print(f"Vector for 'apple': {vector}")
+    print(f"Vector length: {len(vector)}")
+
+    # Compare vector of two words
+    evaluator = load_evaluator("pairwise_embedding_distance")
+    words = ("apple", "iphone")
+    x = evaluator.evaluate_string_pairs(prediction=words[0], prediction_b=words[1])
+    print(f"Comparing ({words[0]}, {words[1]}): {x}")
+
+
+if __name__ == "__main__":
+    main()
