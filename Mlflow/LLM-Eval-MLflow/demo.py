ff4d9c8140c5a2247a2b9b380c5d1468c0ee8e5e
Ankush Singal <andysingal@gmail.com>
2025-05-02 07:59:49 -0500

    Add files via upload

---
+++ Mlflow/LLM-Eval-MLflow/demo.py
@@ -0,0 +1,62 @@
+import mlflow
+import openai
+import os
+import pandas as pd
+import dagshub
+
+dagshub.init(repo_owner='bappymalik4161', repo_name='LLM-Evaluation-with-MLFlow', mlflow=True)
+mlflow.set_tracking_uri("https://dagshub.com/bappymalik4161/LLM-Evaluation-with-MLFlow.mlflow")
+
+
+
+eval_data = pd.DataFrame(
+    {
+        "inputs": [
+            "What is MLflow?",
+            "What is Spark?",
+        ],
+        "ground_truth": [
+            "MLflow is an open-source platform for managing the end-to-end machine learning (ML) "
+            "lifecycle. It was developed by Databricks, a company that specializes in big data and "
+            "machine learning solutions. MLflow is designed to address the challenges that data "
+            "scientists and machine learning engineers face when developing, training, and deploying "
+            "machine learning models.",
+            "Apache Spark is an open-source, distributed computing system designed for big data "
+            "processing and analytics. It was developed in response to limitations of the Hadoop "
+            "MapReduce computing model, offering improvements in speed and ease of use. Spark "
+            "provides libraries for various tasks such as data ingestion, processing, and analysis "
+            "through its components like Spark SQL for structured data, Spark Streaming for "
+            "real-time data processing, and MLlib for machine learning tasks",
+        ],
+    }
+)
+mlflow.set_experiment("LLM Evaluation")
+with mlflow.start_run() as run:
+    system_prompt = "Answer the following question in two sentences"
+    # Wrap "gpt-4" as an MLflow model.
+    logged_model_info = mlflow.openai.log_model(
+        model="gpt-4",
+        task=openai.chat.completions,
+        artifact_path="model",
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": "{question}"},
+        ],
+    )
+
+    # Use predefined question-answering metrics to evaluate our model.
+    results = mlflow.evaluate(
+        logged_model_info.model_uri,
+        eval_data,
+        targets="ground_truth",
+        model_type="question-answering",
+        extra_metrics=[mlflow.metrics.toxicity(), mlflow.metrics.latency(),mlflow.metrics.genai.answer_similarity()]
+    )
+    print(f"See aggregated evaluation results below: \n{results.metrics}")
+
+    # Evaluation result for each data record is available in `results.tables`.
+    eval_table = results.tables["eval_results_table"]
+    df=pd.DataFrame(eval_table)
+    df.to_csv('eval.csv')
+    print(f"See evaluation table below: \n{eval_table}")
+
